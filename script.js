bbb7f3ad8b0e8b8e332d8034ffd8b400df649ff0
k3roropi <139094805+k3roropi@users.noreply.github.com>
2024-01-12 13:40:59 +0800

    add js

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const resume = document.getElementById("resume");
+    const colorButton = document.getElementById("colorButton");
+    let index = 0 
+
+    const colors = ["#2e2e2e", "#3498db"]
+
+    colorButton.addEventListener("click", function () {
+        resume.style.backgroundColor =  colors[index]
+
+        index = index >= colors.length - 1 ? 0 : index + 1;
+    });
+});
